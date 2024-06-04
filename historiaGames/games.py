fd9098a221495fff79202d828b149025c30b6ab8
Adrian-Pardal <139286701+Adrian-Pardal@users.noreply.github.com>
2024-06-04 15:54:07 -0300

    Historia Games
    Fiz um site dinâmico com métodos , classes e id como identificador

---
+++ historiaGames/games.py
@@ -0,0 +1,13 @@
+class Games:
+    def __init__(self , img,banner , id , nome ,desenvolvedor, ano , estilo , tempo , resumo,plataforma):
+        self.img = img
+        self.banner = banner
+        self.id = id
+        self.nome = nome
+        self.desenvolvedor = desenvolvedor
+        self.ano = ano
+        self.estilo = estilo
+        self.tempo = tempo
+        self.resumo = resumo
+        self.plataforma = plataforma
+        
